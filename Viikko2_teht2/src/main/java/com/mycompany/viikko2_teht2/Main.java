1b9a589ef5fad58e0f32db7977fa54cd7e7330b3
prmika <miika.kauppinen@tuni.fi>
2022-01-25 20:09:14 +0200

    Update Main.java

--- Viikko2_teht2/src/main/java/com/mycompany/viikko2_teht2/Main.java
+++ Viikko2_teht2/src/main/java/com/mycompany/viikko2_teht2/Main.java
@@ -5,8 +5,17 @@ public class Main{
 	public static void main( String[] args ){
         Opiskelija lyyli = new Harjoittelija();
 	Opiskelija ville = new Harjoittelija("Ville", "12345", "Gofore", 2000);
+        Harjoittelija testimies= new Harjoittelija();
+        testimies.setNimi("Testimies");
+        
+        Opiskelija kaisa = new Opiskelija("Kaisa", "93848");
+       
        
         ville.tulostaTiedot();
+            System.out.println("----------------");
+       kaisa.tulostaTiedot();
+            System.out.println("----------------");
+            testimies.tulostaTiedot();
             System.out.println("----------------");
         lyyli.tulostaTiedot();
 
